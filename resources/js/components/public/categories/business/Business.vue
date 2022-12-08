a3b30f8c9fea73dd1f85fee58d4a82b08fe3a4b7
kimeu254 <jimkimeu254@gmail.com>
2022-12-08 14:13:59 +0300

    added pagination to views

--- resources/js/components/public/categories/business/Business.vue
+++ resources/js/components/public/categories/business/Business.vue
@@ -5,20 +5,74 @@
         </div>
         <div class="row">
             <div class="col-md-8">
-                <div class="py-4" v-for="news in businessCategory" :key="news.id">
-                    <div class="row" style="background: white;" >
-                        <div class="col-md-6">
-                            <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
-                        </div>
-                        <div class="col-md-6 py-4">
-                            <router-link :to="{name:'viewBusiness', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
-                                {{news.headline}}
-                            </router-link>  
+                <div>
+                    <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                        <div class="py-4" v-for="news in businessCategory.slice(0,6)" :key="news.id"> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
+                                </div>
+                                <div class="col-md-6 py-4">
+                                    <router-link :to="{name:'viewBusiness', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
+                                        {{news.headline}}
+                                    </router-link>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+                        <div class="py-4" v-for="news in businessCategory.slice(6,12)" :key="news.id"> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
+                                </div>
+                                <div class="col-md-6 py-4">
+                                    <router-link :to="{name:'viewBusiness', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
+                                        {{news.headline}}
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-bind:class="{'hidden': openTab !== 3, 'block': openTab === 3}">
+                        <div class="py-4" v-for="news in businessCategory.slice(12,18)" :key="news.id"> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
+                                </div>
+                                <div class="col-md-6 py-4">
+                                    <router-link :to="{name:'viewBusiness', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
+                                        {{news.headline}}
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>              
+                <div>
+                    <nav aria-label="...">
+                        <ul class="pagination justify-content-end">
+                            <li class="page-item">
+                                <a class="page-link" @click="prevTab">Previous</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" v-on:click="toggleTabs(1)" v-bind:class="{'text-dark': openTab !== 1, 'text-dark': openTab === 1}">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" v-on:click="toggleTabs(2)" v-bind:class="{'text-dark': openTab !== 2, 'text-dark': openTab === 2}">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" v-on:click="toggleTabs(3)" v-bind:class="{'text-dark': openTab !== 3, 'text-dark': openTab === 3}">3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" @click="nextTab">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
             <div class="col-md-4">
+                
             </div>
         </div>
     </div>
@@ -30,12 +84,28 @@ import { mapGetters } from 'vuex';
 export default {
     name: 'business',
     data() {
-        return{}
+        return{
+            openTab: 1
+        }
     },
     mounted() {},
     computed: {
         ...mapGetters(["businessCategory"])
     },
+    methods: {
+        toggleTabs(tabNumber){
+            this.openTab = tabNumber
+            window.scrollTo(0,0);
+        },
+        nextTab() {
+            this.openTab++
+            window.scrollTo(0,0);
+        },
+        prevTab() {
+            this.openTab--
+            window.scrollTo(0,0);
+        },
+    }
 }
 </script>
 
@@ -45,5 +115,11 @@ img{
 }
 img:hover{
     cursor: pointer;
+}
+.hidden {
+    display: none;
+}
+.block {
+    display: block;
 }
 </style>
--- resources/js/components/public/categories/politics/Politics.vue
+++ resources/js/components/public/categories/politics/Politics.vue
@@ -5,17 +5,70 @@
         </div>
         <div class="row">
             <div class="col-md-8">
-                <div class="py-4" v-for="news in politicsCategory" :key="news.id">
-                    <div class="row" style="background: white;" >
-                        <div class="col-md-6">
-                            <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
-                        </div>
-                        <div class="col-md-6 py-4">
-                            <router-link :to="{name:'viewPolitics', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
-                                {{news.headline}}
-                            </router-link>
+                <div>
+                    <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                        <div class="py-4" v-for="news in politicsCategory.slice(0,6)" :key="news.id"> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
+                                </div>
+                                <div class="col-md-6 py-4">
+                                    <router-link :to="{name:'viewPolitics', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
+                                        {{news.headline}}
+                                    </router-link>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+                        <div class="py-4" v-for="news in politicsCategory.slice(6,12)" :key="news.id"> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
+                                </div>
+                                <div class="col-md-6 py-4">
+                                    <router-link :to="{name:'viewPolitics', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
+                                        {{news.headline}}
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-bind:class="{'hidden': openTab !== 3, 'block': openTab === 3}">
+                        <div class="py-4" v-for="news in politicsCategory.slice(12,18)" :key="news.id"> 
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img :src="'/storage/posts/' + news.image" alt="" class="img-fluid w-100" style="height: 300px; object-fit: cover;">
+                                </div>
+                                <div class="col-md-6 py-4">
+                                    <router-link :to="{name:'viewPolitics', params:{headline:news.headline}}" class="h4 text-dark" style="text-decoration: none;">
+                                        {{news.headline}}
+                                    </router-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>              
+                <div>
+                    <nav aria-label="...">
+                        <ul class="pagination justify-content-end">
+                            <li class="page-item">
+                                <a class="page-link" @click="prevTab">Previous</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" v-on:click="toggleTabs(1)" v-bind:class="{'text-dark': openTab !== 1, 'text-dark': openTab === 1}">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" v-on:click="toggleTabs(2)" v-bind:class="{'text-dark': openTab !== 2, 'text-dark': openTab === 2}">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" v-on:click="toggleTabs(3)" v-bind:class="{'text-dark': openTab !== 3, 'text-dark': openTab === 3}">3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" @click="nextTab">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
             <div class="col-md-4">
@@ -31,12 +84,28 @@ import { mapGetters } from 'vuex';
 export default {
     name: 'politics',
     data() {
-        return{}
+        return{
+            openTab: 1
+        }
     },
     mounted() {},
     computed: {
         ...mapGetters(["politicsCategory"])
     },
+    methods: {
+        toggleTabs(tabNumber){
+            this.openTab = tabNumber
+            window.scrollTo(0,0);
+        },
+        nextTab() {
+            this.openTab++
+            window.scrollTo(0,0);
+        },
+        prevTab() {
+            this.openTab--
+            window.scrollTo(0,0);
+        },
+    }
 }
 </script>
 
@@ -46,5 +115,11 @@ img{
 }
 img:hover{
     cursor: pointer;
+}
+.hidden {
+    display: none;
+}
+.block {
+    display: block;
 }
 </style>
